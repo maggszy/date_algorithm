bc0f6861b4ce47dd7d7ac89896b56f72a187754a
Magdalena Szymkowiak <77214808+maggszy@users.noreply.github.com>
2021-11-16 00:28:11 +0100

    cosmetical changes and normalising the versions of our code in .ipynb and in .py files

--- list3/L3_Jakubik_Szymkowiak/L3_ZAD1.py
+++ list3/L3_Jakubik_Szymkowiak/L3_ZAD1.py
@@ -1,32 +1,59 @@
 def power(base, power):
-    MOD = 1000000007
+    """
+    Calculate the power using exponentiation by squaring
+    :param base: number which exponentiation is calculated
+    :param power: exponent of the function
+    :return: Number raised to the power
+    """
 
+    if power < 0:
+        raise ValueError("Invalid value")
+        
+    if not isinstance(power, int):
+        raise TypeError("Power should be an integer.")
+        
+    if power == 0:
+        return 1
+    
     result = 1
     while power > 0:
         # If power is odd
         if power % 2 == 1:
-            result = (result * base) % MOD
+            result = (result * base)
 
         # Divide the power by 2
         power = power // 2
         # Multiply base to itself
-        base = (base * base) % MOD
+        base = (base * base)
 
     return result
 
 def probability(n, k, p):
+    """
+    Calculate the Binomial Distribution of k successed in n trials with p probability of success.
+    
+    :param n: amount of trials
+    :param k: amount of successes
+    :param p: probability of success
+    :return: tuple with wanted probability and number of executed multiplications
+    """
+    
+    if p < 0 or p >1:
+        raise ValueError("Probability has a value between 0 and 1")
+    
     x=p/(1-p) 
     
     prob=1
-    binom_all_x=n*x
-    count_mult=2
+    binom_all_x=n*x #for i=1
+    count_mult=2 #one multiplication done in x and one in binom_all_x
     
     for i in range(2,k+1):
-        binom_all_x*=(n+1-i)/i*x
+        binom_all_x*=(n+1-i)/i*x #3 mult
         prob+=binom_all_x
         count_mult+=3
     
     factor = power(1-p,n)
     prob *= factor
-    count_mult+=1
+    count_mult+=1 #1 mult done in return
+    
     return (prob, count_mult)
--- list3/L3_Jakubik_Szymkowiak/L3_ZAD2.py
+++ list3/L3_Jakubik_Szymkowiak/L3_ZAD2.py
@@ -7,6 +7,15 @@ def ordinary_polynomial_value_calc(coeff, arg):
     :param arg: the value of variable x in polynomial W(x)
     :return: tuple with the value of polynomial and amout of executed multiplication and addition
     """
+    if not isinstance(coeff,list):
+        raise TypeError("Variable 'coeff' should be a list")
+        
+    if type(arg) not in {int,float}:
+        raise TypeError("Variable 'arg' should be an int or float")
+    
+    if coeff==[]:
+        raise ValueError("Lack of coefficients, fill list with numvers!")
+    
     n=len(coeff)
     count_add=0
     count_mult=0
@@ -24,6 +33,15 @@ def smart_polynomial_value_calc(coeff, arg):
     :param arg: the value of variable x in polynomial W(x)
     :return: tuple with the value of polynomial and amout of executed multiplication and addition
     """
+    if not isinstance(coeff,list):
+        raise TypeError("Variable 'coeff' should be a list")
+        
+    if type(arg) not in {int,float}:
+        raise TypeError("Variable 'arg' should be an int or float")
+    
+    if coeff==[]:
+        raise ValueError("Lack of coefficients, fill list with numvers!")
+        
     n=len(coeff)
     coeff.reverse() 
     value=coeff[0]
@@ -33,44 +51,3 @@ def smart_polynomial_value_calc(coeff, arg):
         count_mult+=1
     count_add=count_mult
     return value, count_mult, count_add
-
-
-#the same programs as above, but with counting time
-
-def ordinary_polynomial_value_calc_time(coeff, arg):
-    """
-    Calculate value of polynomial in a standart way
-    :param coeff: a list with coefficients of polynomial positioned from free term to the term of the highest degree
-    :param arg: the value of variable x in polynomial W(x)
-    :return: tuple with the value of polynomial, amout of executed multiplication and addition and time needed to execute program
-    """
-    start=time.time()
-    n=len(coeff)
-    count_add=0
-    count_mult=0
-    value=coeff[0]
-    for dg in range(1,n):
-        value=coeff[dg]*(arg**dg) + value
-        count_mult+=dg
-        count_add+=1
-    finish=time.time()
-    return value, count_mult, count_add, finish-start
-
-def smart_polynomial_value_calc_time(coeff, arg):
-    """
-    Calculate value of polynomial using the Horner's algorithm
-    :param coeff: a list with coefficients of polynomial positioned from free term to the term of the highest degree
-    :param arg: the value of variable x in polynomial W(x)
-    :return: tuple with the value of polynomial, amout of executed multiplication and addition and time needed to execute program
-    """
-    start=time.time()
-    n=len(coeff)
-    coeff.reverse() 
-    value=coeff[0]
-    count_mult=0
-    for dg in range(1,n): #dg=degree
-        value = value*arg + coeff[dg]
-        count_mult+=1
-    count_add=count_mult
-    finish=time.time()
-    return value, count_mult, count_add, finish-start
