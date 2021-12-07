fec8b9bc3a93a5ed6e521a4e3d288d6fc51ffa8f
Magdalena Szymkowiak <77214808+maggszy@users.noreply.github.com>
2021-12-07 21:56:18 +0100

    another final files

--- list4/L4_ZAD4.py
+++ list4/L4_ZAD4.py
@@ -1,28 +1,57 @@
 class Stack():
     """
-    Stos
+    Klasa implementująca stos używając pythonowych list. 
     """
 
     def __init__(self):
         self.list_of_items = []
         
     def push(self, item):
+        """
+        Metoda służąca do dodawania obiektu do stosu.
+        Pobiera jako argument obiekt który ma być dodany.
+        Niczego nie zwraca.
+        """
         self.list_of_items.append(item)
     
     def peek(self):
+        """
+        Metoda służąca do podawania jaki obiekt znajduje się na szczycie stosu.
+        Nie pobiera argumentów.
+        Zwraca obiekt znajdujący się na szczycie stosu, ale go stamtąd nie ściąga.
+        """
         return self.list_of_items[len(self.list_of_items)-1]
         
     def pop(self):
+        """
+        Metoda służąca do ściągania obiektów ze stosu.
+        Nie pobiera argumentów.
+        Zwraca ściągnięty obiekt.
+        """
         return self.list_of_items.pop()
     
     def is_empty(self):
+        """
+        Metoda służąca do sprawdzania, czy stos jest pusty.
+        Nie pobiera argumentów.
+        Zwraca True jeśli stos jest pusty lub False gdy nie jest.
+        """
         return self.list_of_items==[]
         
     def size(self):
+        """
+        Metoda służąca do określania wielkości stosu.
+        Nie pobiera argumentów.
+        Zwraca liczbę obiektów w stosie.
+        """
         return len(self.list_of_items)
 
-    def show(self):
-        return print(self.list_of_items) 
+    def __str__(self):
+        """
+        Medota odpowiadająca za nieformalną reprezentacje kolejki.
+        Nie pobiera argumentów.
+        """
+        return str(self.list_of_items) 
 
 
 def checking_HTML_correctness(filename):
@@ -70,6 +99,6 @@ def checking_HTML_correctness(filename):
     else:
         return False
 
-#a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_3.txt'))
-#a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_2.txt'))
-#a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_1.txt'))
+#a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_3.txt'))  #False
+#a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_2.txt'))  #False
+#a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_1.txt'))  #True
