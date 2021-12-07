c44967b8eb9f1c4073f12a044c7b6bd77f7d821d
Małgorzata Jakubik <262279@student.pwr.edu.pl>
2021-12-07 04:13:49 +0100

    add part of 3

--- list4/L4_ZAD3.py
+++ list4/L4_ZAD3.py
@@ -57,12 +57,68 @@ class Stack:
     def size(self):
         return len(self.items)
 
+######################################
+# Modelowanie ewakuacji uczesników wydarzenia autokarami.
+# Założenia:
+# Wszyscy jadą w to samo miejsce, autokar nie zatrzmuje się na przystankach na trasie, każdy uczestnik wydarzenia wraca autokarem.
 
-class Bus(object):
-    def __init__(self):
+
+######################################
+
+class Bus:
+    def __init__(self, ppm):
+        self.newbus = ppm # jak często autobus podjeżdża na przystanek
+        self.currentDrive = None
+        self.timeRemaining = 0 # czas oczekiwania na kolejny kurs autobusu
+        self.bus_capacity = 25
+
+    def tick(self):
+        if self.currentDrive != None:
+            self.timeRemaining = self.timeRemaining - 1
+        if self.timeRemaining <= 0:
+            self.currentDrive = None
+
+    def on_the_road(self):
+        if self.currentDrive != None:
+            return True
+        else:
+            return False
+
+    def onboard(self):
+        # return how much passengers is on board
         pass
 
 
-class Simulation(object):
-    def __init__(self):
-        pass
+class Passenger:
+    def __init__(self, geton_time, rate):
+        self.geton_time = geton_time
+        self.rate = rate # w jakim tempie ludzie przychodza na przystanek
+        self.person_id = 0
+
+    def getStamp(self):
+        return self.timestamp
+
+    def waitTime(self, currenttime):
+        return currenttime - self.timestamp
+
+
+def simulation(numSeconds, amount_of_doors):
+    bus = Bus(amount_of_doors)
+    group = Queue()
+    waitingtimes = []
+
+    for currentSecond in range(numSeconds):
+        if Bus.on_the_road() < Bus.bus_capacity:
+            passenger = Passenger(currentSecond)
+            group.enqueue(passenger)
+
+    if (not bus.on_the_road()) and (not group.isEmpty()):
+        nexttask = group.dequeue()
+        waitingtimes.append(nexttask.waitTime(currentSecond))
+        bus.startNext(nexttask)
+
+    bus.tick()
+
+    # averageWait = sum(waitingtimes) / len(waitingtimes)
+    # print("Average Wait %6.2f secs %3d tasks remaining." % (averageWait, group.size()))
+
