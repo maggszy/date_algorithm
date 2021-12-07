e0a87d5813093d8ab55ddebd712f5f1e8061e2cc
Magdalena Szymkowiak <77214808+maggszy@users.noreply.github.com>
2021-12-07 18:43:06 +0100

    final version of 4

--- list4/L4_ZAD4.py
+++ list4/L4_ZAD4.py
@@ -6,13 +6,13 @@ class Stack():
     def __init__(self):
         self.list_of_items = []
         
-    def enqueue(self, item):
+    def push(self, item):
         self.list_of_items.append(item)
     
     def peek(self):
         return self.list_of_items[len(self.list_of_items)-1]
         
-    def dequeue(self):
+    def pop(self):
         return self.list_of_items.pop()
     
     def is_empty(self):
@@ -34,13 +34,13 @@ def checking_HTML_correctness(filename):
 
     file_obj = open(filename, 'r')
     text = list(file_obj.read())
-    ignore=['!DOCTYPE','area','base','br','br','col','command','embed','hr','img','input','keygen','link','meta','param','source','track','wbr']
+    ignore=['!DOCTYPE','area','base','br','br','col','command','embed','hr','img','input','keygen','link','meta','param','source','track','wbr','!--']
     stack_tags=Stack()
     stack_brackets=Stack()
 
     for i in range(len(text)):
         if text[i]=="<":
-            stack_brackets.enqueue(text[i])
+            stack_brackets.push(text[i])
             i+=1
 
             tag=''
@@ -50,28 +50,26 @@ def checking_HTML_correctness(filename):
                 
             tag= tag.split(' ')[0]
             if stack_tags.is_empty() and (tag not in ignore):
-                stack_tags.enqueue(tag)
+                stack_tags.push(tag)
             elif tag in ignore:
                 pass
             elif  stack_tags.is_empty()==False and (tag not in ignore):
                 if tag == '/'+ stack_tags.peek():
-                    stack_tags.dequeue()
-            else:
-                stack_tags.enqueue(tag)
+                    stack_tags.pop()
+                else:
+                    stack_tags.push(tag)
         
         elif text[i]==">":
             if stack_brackets.is_empty():
                 return False
             elif stack_brackets.peek()=="<":
-                stack_brackets.dequeue()
+                stack_brackets.pop()
 
     if stack_brackets.is_empty() and stack_tags.is_empty():
         return True
     else:
         return False
 
-a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_3.txt'))
+#a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_3.txt'))
 #a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_2.txt'))
 #a=print(checking_HTML_correctness('L4_ZAD4_sampleHTML_1.txt'))
-#a=print(checking_HTML_correctness('prawdo.txt'))
-#a=print(checking_HTML_correctness('moje.txt'))
