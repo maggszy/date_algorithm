d8309f616bd0127d19b3e9ee8fa2878f1f4db94e
Magdalena Szymkowiak <77214808+maggszy@users.noreply.github.com>
2022-01-25 22:53:42 +0100

    adding to task3

---
+++ list7/L7_ZAD3.py
@@ -0,0 +1,116 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+class Vertex:
+    def __init__(self,num):
+        self.id = num
+        self.connectedTo = {}
+        self.color = 'white'       #new: color of node
+        self.dist = sys.maxsize    #new: distance from beginning (will be used later)
+        self.pred = None           #new: predecessor
+        self.disc = 0              #new: discovery time
+        self.fin = 0               #new: end-of-processing time
+
+    def addNeighbor(self,nbr,weight=0):
+        self.connectedTo[nbr] = weight
+        
+    def setColor(self,color):
+        self.color = color
+        
+    def setDistance(self,d):
+        self.dist = d
+
+    def setPred(self,p):
+        self.pred = p
+
+    def setDiscovery(self,dtime):
+        self.disc = dtime
+        
+    def setFinish(self,ftime):
+        self.fin = ftime
+        
+    def getFinish(self):
+        return self.fin
+        
+    def getDiscovery(self):
+        return self.disc
+        
+    def getPred(self):
+        return self.pred
+        
+    def getDistance(self):
+        return self.dist
+        
+    def getColor(self):
+        return self.color
+    
+    def getConnections(self):
+        return self.connectedTo.keys()
+        
+    def getWeight(self,nbr):
+        return self.connectedTo[nbr]
+                
+    def __str__(self):
+        return str(self.id) + ":color " + self.color + ":disc " + str(self.disc) + ":fin " + str(self.fin) + ":dist " + str(self.dist) + ":pred \n\t[" + str(self.pred)+ "]\n"
+    
+    def getId(self):
+        return self.id
+
+
+#przeszukiwanie wszerz
+def bfs(g,start):
+    start.setDistance(0)                            #distance 0 indicates it is a start node
+    start.setPred(None)                             #no predecessor at start
+    vertQueue = Queue()
+    vertQueue.enqueue(start)                        #add start to processing queue
+    while (vertQueue.size() > 0):
+        currentVert = vertQueue.dequeue()           #pop next node to process -> current node
+        for nbr in currentVert.getConnections():    #check all neighbors of the current node
+            if (nbr.getColor() == 'white'):         #if the neighbor is white
+                nbr.setColor('gray')                             #change its color to grey
+                nbr.setDistance(currentVert.getDistance() + 1)   #set its distance
+                nbr.setPred(currentVert)                         #current node is its predecessor
+                vertQueue.enqueue(nbr)                           #add it to the queue
+        currentVert.setColor('black')               #change current node to black after visiting all of its neigh.
+
+
+#przeszukiwanie w głąb
+
+class DFSGraph(Graph):
+    def __init__(self):
+        super().__init__()
+        self.time = 0
+
+    def dfs(self):
+        for aVertex in self:
+            aVertex.setColor('white')
+            aVertex.setPred(-1)
+        for aVertex in self:
+            if aVertex.getColor() == 'white':
+                self.dfsvisit(aVertex)
+
+    def dfsvisit(self,startVertex):
+        startVertex.setColor('gray')
+        self.time += 1
+        startVertex.setDiscovery(self.time)
+        for nextVertex in startVertex.getConnections():
+            if nextVertex.getColor() == 'white':
+                nextVertex.setPred(startVertex)
+                self.dfsvisit(nextVertex)
+        startVertex.setColor('black')
+        self.time += 1
+        startVertex.setFinish(self.time)
